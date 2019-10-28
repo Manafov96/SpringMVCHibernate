f4fb2d42296061519194959bdc998e233278274d
Manafov96 <Viko@Viko-PC>
2019-10-28 09:56:34 +0200

    Added Login and Register functionality.

--- src/main/java/com/springBootMvcHibernate/controllers/ProductController.java
+++ src/main/java/com/springBootMvcHibernate/controllers/ProductController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.ui.ModelMap;
+//import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,15 +20,15 @@ public class ProductController {
 
 	@Autowired
 	private ProductService service; 
-	
+/*	
 	@RequestMapping(value = "/login", method=RequestMethod.GET)
 	public String showLoginPage(ModelMap model) {
 		model.put("name", "user");
 		model.put("name", "Viko");
 		return "index";
 	}
-	
-	@RequestMapping("/")
+*/	
+	@RequestMapping("/index")
 	public String viewHomePage(Model model) {
 		List<Product> listProducts = service.listAll();
 		model.addAttribute("listProducts", listProducts);
@@ -48,7 +48,7 @@ public class ProductController {
 	public String saveProduct(@ModelAttribute("product") Product product) {
 		service.save(product);
 		
-		return "redirect:/";
+		return "redirect:/index";
 	}
 	
 	@RequestMapping("/edit/{id}")
@@ -63,6 +63,6 @@ public class ProductController {
 	@RequestMapping("/delete/{id}")
 	public String deleteProduct(@PathVariable(name = "id") int id) {
 		service.delete(id);
-		return "redirect:/";		
+		return "redirect:/index";		
 	}
 }
--- src/main/java/com/springBootMvcHibernate/security/SecurityConfiguration.java
+++ src/main/java/com/springBootMvcHibernate/security/SecurityConfiguration.java
@@ -1,30 +1,51 @@
 package com.springBootMvcHibernate.security;
 
+import javax.sql.DataSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
-	@SuppressWarnings("deprecation")
-	@Configuration
-	@EnableWebSecurity
-	public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-		@Autowired
-		public void configureGlobalSecurity(AuthenticationManagerBuilder auth)
-				throws Exception {
-			auth.inMemoryAuthentication().passwordEncoder(NoOpPasswordEncoder.getInstance()).
-			withUser("user").password("password")
-					.roles("USER", "ADMIN");
-		}
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-		@Override
-		protected void configure(HttpSecurity http) throws Exception {
-			http.authorizeRequests().antMatchers("/*").permitAll()
-					.antMatchers("/*").access("hasRole('USER')").and()
-					.formLogin();
-		}
+	@Autowired
+	private DataSource dataSource;
+
+	private final String USERS_QUERY = "select email, password, active from user where email=?";
+	private final String ROLES_QUERY = "select u.email, r.role from user u inner join user_role ur on (u.id = ur.user_id) inner join role r on (ur.role_id=r.role_id) where u.email=?";
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().usersByUsernameQuery(USERS_QUERY).authoritiesByUsernameQuery(ROLES_QUERY)
+				.dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll().antMatchers("/signup")
+				.permitAll().antMatchers("/**").hasAuthority("ADMIN").anyRequest().authenticated().and().csrf()
+				.disable().formLogin().loginPage("/login").failureUrl("/login?error=true").defaultSuccessUrl("/home")
+				.usernameParameter("email").passwordParameter("password").and().logout().logoutSuccessUrl("/").and()
+				.rememberMe();
+	}
+
+	@Bean
+	public PersistentTokenRepository persistentTokenRepository() {
+		JdbcTokenRepositoryImpl db = new JdbcTokenRepositoryImpl();
+		db.setDataSource(dataSource);
+
+		return db;
+	}
+}
